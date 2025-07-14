6bfeb8f5acbf68d9d679336c979445d4ae75e084
yuvarajgit-2505 <yuvaraj25525@gmail.com>
2025-07-14 13:21:12 +0530

    Variables

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -4,10 +4,8 @@ package $basic_java;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
